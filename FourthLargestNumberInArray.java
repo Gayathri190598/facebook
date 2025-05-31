7e3484e1a13e3474a8e494de969aa64ccf50827b
Gayathri190598 <rgayu1998@gmail.com>
2025-05-31 21:49:53 +0530

    add sum of elements a and b and stored in sum variable.

--- FourthLargestNumberInArray.java
+++ FourthLargestNumberInArray.java
@@ -5,6 +5,8 @@ public class FourthLargestNumberInArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> array=new ArrayList<Integer>();
+		int a=10,b=20;
+		int sum=a+b;
 			
 		}
 
